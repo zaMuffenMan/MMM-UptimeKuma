0bad782f5e59a2a9b8fea4cce763779d1e7ed73c
Mark Nuns <mark@nunsfamily.com>
2023-01-27 22:28:02 +0000

    Removed node_helper.js

--- MMM-UptimeKuma.js
+++ MMM-UptimeKuma.js
@@ -47,6 +47,47 @@ Module.register('MMM-UptimeKuma', {
         })
 
     return wrapper;
-  }
+    },
+
+    socketNotificationReceived: function (notification, payload) {
+        var self = this;
+
+        switch (notification) {
+            case 'MMM-UPTIMEKUMA-CONFIG':
+                self.handleConfigNotification(payload);
+            case 'MMM-UPTIMEKUMA-GET':
+                self.handleGetNotification(payload);
+        }
+    },
+
+    handleConfigNotification: function (payload) {
+        var self = this;
+        self.config = payload;
+    },
+
+    handleGetNotification: function (payload) {
+        getServices(payload);
+    },
+
+    // Get data from the services
+    getServices: function (config) {
+        var self = this;
+
+        clearTimeout(this.timer);
+        self.timer = null;
+
+        self.serviceResults = [];
+        config.services.forEach(service => {
+            var obj = {};
+            obj['data'] = config.baseUrl + '/api/badge/' + service.id + '/status';
+            obj['label'] = service.label;
+            self.serviceResults.push(obj);
+        });
+
+        // Set check times
+        self.timer = setTimeout(function () {
+            self.handleGetNotification(self.config)
+        }, 60 * 1000); // update once a minute
+    },
 
 });
